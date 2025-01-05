8532a215b3300c49c6067659b66fae6b04b41b0e
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-01-05 15:28:22 +0900

    [Silver III] Title: N과 M (1), Time: 168 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,25 +1,20 @@
-from collections import deque 
-import sys
-n, m = map(int, sys.stdin.readline().split())
+from collections import deque
+n, m = map(int, input().split(' '))
 
-de = deque([])
-
+q = deque()
 
 
-def dfs(depth):
-    if depth > m:
-        print(' '.join(str(num) for num in de))
-        de.pop()
-        depth -= 1
+def dfs():
+    if len(q) == m:
+        print(' '.join(map(str, q)))
         return
-    for i in range(1, n+1):
-        if i in de:
-            continue
-        de.append(i)
-        dfs(depth+1)
-    if de:
-        de.pop()
-dfs(1)
-        
 
-    
+    for i in range(1, n+1):
+        if i in q:
+            continue
+
+        q.append(i)
+        dfs()
+        q.pop()
+
+dfs()
