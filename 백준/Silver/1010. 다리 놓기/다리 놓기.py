92a2e22fe6c0fa8a4be47b31b7c775db5ce9fa2b
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-01-13 23:35:18 +0900

    [Silver V] Title: 다리 놓기, Time: 56 ms, Memory: 34536 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,9 +1,9 @@
+import math
+
 t = int(input())
 
 for _ in range(t):
     n, m = map(int, input().split())
-    dam = 1
-    for i in range(n):
-        dam *= m-i
-        dam/=1+i
-    print(int(dam))
+    print(math.comb(m, n))
+
+
