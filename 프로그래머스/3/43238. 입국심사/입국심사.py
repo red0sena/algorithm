0751e561519d6e6277d221248cd138f33ec00d32
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-01-31 01:21:52 +0900

    [level 3] Title: 입국심사, Time: 481.70 ms, Memory: 14.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -0,0 +1,24 @@
+def solution(n, times):
+    min_time = min(times)
+    
+    low, high = 0, min_time * n
+    
+    answer = high  
+    
+    while low <= high:
+        mid = (low + high) // 2
+        
+        count = 0
+        for t in times:
+            count += mid // t
+            if count >= n:
+                break
+        
+        if count >= n:
+            answer = mid
+            high = mid - 1
+        else:
+
+            low = mid + 1
+    
+    return answer
