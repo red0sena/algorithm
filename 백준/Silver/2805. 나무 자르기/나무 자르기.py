749943a54005b85206f4f66d3e55ff9fd83d7720
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-01-31 03:48:24 +0900

    [Silver II] Title: 나무 자르기, Time: 924 ms, Memory: 150292 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,26 +1,42 @@
+import sys
+import bisect
+
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
+trees = list(map(int, input().split()))
 
-input_list = list(map(int, input().split()))
+# 1) 정렬
+trees.sort()
 
+# 2) 누적합(prefix sum) 계산
+prefix = [0] * (n+1)
+for i in range(1, n+1):
+    prefix[i] = prefix[i-1] + trees[i-1]
+    
+# 이분 탐색 범위 설정
+left, right = 0, trees[-1]
+answer = 0
 
-max_val = max(input_list) - 1
-min_val = 0
-res = max_val
-
-while min_val <= max_val:
-    mid = (min_val+max_val) // 2
-    tree_m = 0
-    for tree in input_list:
-        if tree >= mid:
-            tree_m += (tree - mid)
-
-    if tree_m >= m:
-        min_val = mid + 1
-        res = mid
+while left <= right:
+    mid = (left + right) // 2
+    
+    # 3) mid 초과 구간 찾기: 이분 탐색 (bisect)
+    #  trees에서 mid보다 큰 구간의 시작 index
+    idx = bisect.bisect_right(trees, mid)  
+    # trees[idx] 이상인 원소들이 mid보다 큼
+    
+    # 4) mid보다 큰 나무들 합 구하기
+    #    나무들을 모두 더해놓은 prefix[N] - prefix[idx] = (trees[idx] + ... + trees[N-1])의 합
+    #    각 나무는 mid 이상이므로, 잘린 양은 (합 - mid * (개수))
+    #   개수는 (n - idx)
+    cut_sum = prefix[n] - prefix[idx] - mid * (n - idx)
+    
+    # 이 cut_sum이 우리가 필요한 나무(m)보다 크거나 같으면 -> 더 위에서 잘라도 됨
+    if cut_sum >= m:
+        answer = mid  # 만족 가능한 mid, 갱신
+        left = mid + 1
     else:
-        max_val = mid - 1
+        right = mid - 1
 
-
-print(res)
-
-
+print(answer)
