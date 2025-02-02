1e6739f03057b658a5ab9dd736a2d52d870d382a
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-02-03 00:43:16 +0900

    [Silver III] Title: 수열, Time: 72 ms, Memory: 40120 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.py
+++ 백준/Silver/2559. 수열/수열.py
@@ -1,15 +1,23 @@
 import sys
-n, k = map(int, sys.stdin.readline().rstrip().split())
+input = sys.stdin.readline
 
-input_list = list(map(int, sys.stdin.readline().rstrip().split()))
-prefix = [0]
-for i in range(n):
-    prefix.append(prefix[i] + input_list[i])
+# 1) N, K 입력 받기
+N, K = map(int, input().split())
+# 2) 온도 리스트 입력 받기
+temps = list(map(int, input().split()))
 
-res = -int(10e10)
-for i in range(k, n+1):
-    a = prefix[i] - prefix[i-k]
-    res = max(res, a)
+# 3) 처음 K개의 합을 구함
+current_sum = sum(temps[:K])
+max_sum = current_sum
 
-print(res)
+# 4) 슬라이딩 윈도우(투 포인터)로 최댓값 찾기
+#    i는 오른쪽 포인터를 의미, i가 한 칸씩 오른쪽으로 이동
+for i in range(K, N):
+    # 윈도우에서 빠져나가는 값 빼고, 새로 들어오는 값 더하기
+    current_sum = current_sum - temps[i-K] + temps[i]
+    # 최대합 갱신
+    if current_sum > max_sum:
+        max_sum = current_sum
 
+# 5) 결과 출력
+print(max_sum)
