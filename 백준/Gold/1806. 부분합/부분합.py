4a77d821503912c98a2f8ecfb33c9ae036223941
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-02-06 02:59:29 +0900

    [Gold IV] Title: 부분합, Time: 100 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,28 +1,24 @@
-import sys
+n, s = map(int, input().split())
 
-input = sys.stdin.readline
-# 입력 받기
-N, S = map(int, input().split())
-arr = list(map(int, input().split()))
+input_list = list(map(int, input().split()))
 
-# 투 포인터를 이용한 풀이
-start, end = 0, 0
-current_sum = 0
-answer = float('inf')
+
+
+start, end, current_val = 0, 0, 0
+min_len = 100000001
 
 while True:
-    # 현재 합이 S 미만이고, end가 범위 안에 있다면
-    if current_sum < S and end < N:
-        current_sum += arr[end]
+    if current_val < s and end < n:
+        current_val += input_list[end]
         end += 1
-    # 현재 합이 S 이상이면 최솟값 갱신 후 start를 이동
-    elif current_sum >= S:
-        answer = min(answer, end - start)
-        current_sum -= arr[start]
+    elif current_val >= s:
+        min_len  = min(min_len, end-start)
+        current_val -= input_list[start]
         start += 1
     else:
-        # 더 이상 탐색할 수 없는 경우 종료
         break
 
-# 만약 갱신되지 않았다면 0 출력, 그렇지 않다면 answer 출력
-print(0 if answer == float('inf') else answer)
+if min_len == 100000001:
+    print(0)
+else:
+    print(min_len)
