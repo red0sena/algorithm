35f443340c5e4e83d1620be3bd734860990d8179
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-02-07 15:06:04 +0900

    [Gold IV] Title: 미로만들기, Time: 64 ms, Memory: 34944 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,31 +1,28 @@
 from collections import deque
 
 n = int(input())
+
 input_list = [list(map(int, input())) for _ in range(n)]
 
+visited = [[-1] * n for _ in range(n)]
+move = [(0,1),(1,0),(-1,0),(0,-1)]
+q = deque([(0,0)])
+visited[0][0] = 0
+while q:
+    x, y = q.popleft()
+    if x == n-1 and y == n-1:
+        print(visited[x][y])
+        break
+    for dx, dy in move:
+        nx = dx + x
+        ny = dy + y
 
-move = [(0,1),(1,0),(0,-1),(-1,0)]
-
-def bfs():
-    visited = [[-1] * n for _ in range(n)]
-    visited[0][0] = 0
-    q = deque([(0,0)])
-    flag=False
-    while q:
-        x, y = q.popleft()
-        if x == n - 1 and y == n - 1:
-            return visited[x][y]
-
-        for dx, dy in move:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < n and 0 <= ny < n and visited[nx][ny] == -1:
+        if 0 <= nx < n and 0 <= ny < n:
+            if visited[nx][ny] == -1:
                 if input_list[nx][ny] == 1:
-                    q.appendleft((nx, ny))
                     visited[nx][ny] = visited[x][y]
+                    q.appendleft((nx,ny))
                 else:
-                    q.append((nx, ny))
                     visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx,ny))
 
-
-print(bfs())
