97fa4b0a862fc0ecf54b2d81911c5ee9ec436aba
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-02-07 15:32:35 +0900

    [level 3] Title: 등굣길, Time: 3.87 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.py
+++ 프로그래머스/3/42898. 등굣길/등굣길.py
@@ -1,25 +1,13 @@
-from collections import deque
 def solution(m, n, puddles):
-    map_list = [[1] * m for _ in range(n)]
-    visited = [[0] * m for _ in range(n)]
-    visited[0][0] = 1
-    for puddle in puddles:
-        map_list[puddle[1] - 1][puddle[0] - 1] = 0
-    move = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+    puddles = [[q,p] for [p,q] in puddles]      # 미리 puddles 좌표 거꾸로
+    dp = [[0] * (m + 1) for i in range(n + 1)]  # dp 초기화 # 기존보다 한칸 더 생성하여 OOR를 예방한다.
+    dp[1][1] = 1           # 집의 위치(시작위치)
 
-    q = deque([(0,0)])
-
-    while q:
-        x, y = q.popleft()
-        for dx, dy in move:
-            nx = x + dx
-            ny = y  + dy
-            if 0 <= nx < n and 0 <= ny < m:
-                if map_list[nx][ny] == 1:
-                    map_list[nx][ny] = -1
-                    q.append((nx,ny))
-        visited[x][y] += visited[x-1][y]
-        visited[x][y] += visited[x][y-1]
-        visited[x][y] %= 1000000007
-
-    return visited[-1][-1]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1 and j == 1: continue # 시작점은 넘어간다.
+            if [i, j] in puddles:    # 웅덩이 위치의 경우 값을 0으로
+                dp[i][j] = 0
+            else:                    # 현재 칸은 왼쪽 칸, 위 칸의 합산!
+                dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007
+    return dp[n][m]
