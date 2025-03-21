cd4ca5fa0fa7bc0691e80687c8adde230803eeef
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-03-21 17:31:17 +0900

    [level 3] Title: 연속 펄스 부분 수열의 합, Time: 514.19 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
+++ 프로그래머스/3/161988. 연속 펄스 부분 수열의 합/연속 펄스 부분 수열의 합.py
@@ -1,28 +1,25 @@
 def max_subarray_sum(arr):
     current_sum = arr[0]
     max_sum = arr[0]
-    for i in range(1, len(arr)):
-        current_sum = max(arr[i], current_sum + arr[i])
-        max_sum = max(max_sum, current_sum)
-    return max_sum
-
-
-def solution(sequence):
-    prefix1 = [0]
     
+    for i in range(1, len(arr)):
+        current_sum = max(arr[i], arr[i]+current_sum)
+        max_sum = max(max_sum, current_sum)
+    print(max_sum)
+    return max_sum
+def solution(sequence):
+    pers1_sequence = []
+    pers2_sequence = []
     for i in range(len(sequence)):
         if i % 2 == 0:
-            prefix1.append(sequence[i])
+            pers1_sequence.append(-sequence[i])
+            pers2_sequence.append(sequence[i])
         else:
-            prefix1.append((-sequence[i]))
-    a = (max_subarray_sum(prefix1))
-    prefix2 = [0]
+            pers1_sequence.append(sequence[i])
+            pers2_sequence.append(-sequence[i])
     
-    for i in range(len(sequence)):
-        if i % 2 != 0:
-            prefix2.append(sequence[i])
-        else:
-            prefix2.append((-sequence[i]))
-    b = (max_subarray_sum(prefix2))
-    # print(prefix2, prefix1)
-    return max(a,b)
+    pers1 = max_subarray_sum(pers1_sequence)
+    pers2 = max_subarray_sum(pers2_sequence)
+            
+            
+    return max(pers1, pers2) 
