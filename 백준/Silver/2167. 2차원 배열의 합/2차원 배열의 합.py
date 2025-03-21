827f0e9dd2a6b2f4843aaef70877ceca86170b81
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-03-21 18:16:43 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 112 ms, Memory: 38948 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.py
@@ -1,24 +1,30 @@
+import sys
+input = sys.stdin.readline
+
 n, m = map(int, input().split())
 
 input_list = [list(map(int, input().split())) for _ in range(n)]
 
-k = int(input())
-
-
 prefix = [[0] * (m+1) for _ in range(n+1)]
+
 
 for i in range(1, n+1):
     for j in range(1, m+1):
-        prefix[i][j] = (prefix[i-1][j]
-                        + prefix[i][j-1]
-                        - prefix[i-1][j-1]
-                        + input_list[i-1][j-1])
+        prefix[i][j] = prefix[i-1][j] + prefix[i][j-1] + input_list[i-1][j-1] - prefix[i-1][j-1]
+
+
+k = int(input())
 
 for _ in range(k):
-    i, j, x, y = map(int, input().split())
+    x1, y1, x2, y2 = map(int, input().split())
 
-    ans = (prefix[x][y]
-           - prefix[x][j - 1]
-           - prefix[i - 1][y]
-           + prefix[i - 1][j - 1])
+    ans = (
+        prefix[x2][y2]
+        - prefix[x2][y1-1]
+        - prefix[x1-1][y2]
+        + prefix[x1-1][y1-1]
+    )
     print(ans)
+
+
+
