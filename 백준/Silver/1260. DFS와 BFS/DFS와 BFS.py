09db70d690756a4bc6aecba6c61870e010b41c51
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-04-08 21:02:45 +0900

    [Silver II] Title: DFS와 BFS, Time: 68 ms, Memory: 35708 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -1,43 +1,50 @@
-import sys
 from collections import deque
+import sys
 
-n, m, v = map(int, sys.stdin.readline().rstrip().split(' '))
-node_list = [[] for _ in range(n)]
-visited = [False] * n
+input = sys.stdin.readline
 
-for _ in range(m):
-    node, edge = map(int, sys.stdin.readline().rstrip().split(' '))
-    node_list[node-1].append(edge-1)
-    node_list[edge - 1].append(node - 1)
+# 입력 받기
+N, M, V = map(int, input().split())
 
-for i in range(n):
-    node_list[i] = sorted(node_list[i])
+# 그래프를 dictionary로 초기화 (정점 번호: 인접 정점 리스트)
+graph = {i: [] for i in range(1, N+1)}
 
-## DFS ##
-def dfs(node):
-    if visited[node] == True:
-        return
-    visited[node] = True
+# 간선 정보를 양방향으로 추가
+for _ in range(M):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
 
-    print(node+1, end=' ')
-    for next_node in node_list[node]:
-        dfs(next_node)
+# 방문 순서가 작은 정점부터 탐색할 수 있도록 정렬
+for key in graph:
+    graph[key].sort()
 
-dfs(v-1)
-print()
-## BFS ##
-visited = [False] * n
+# DFS 구현 (재귀 방식)
+def dfs(v, visited, dfs_result):
+    visited.add(v)
+    dfs_result.append(v)
+    for neighbor in graph[v]:
+        if neighbor not in visited:
+            dfs(neighbor, visited, dfs_result)
 
-q = deque()
-q.append(v-1)
-visited[v-1] = True
+dfs_result = []
+visited_dfs = set()
+dfs(V, visited_dfs, dfs_result)
 
-while q:
-    node = q.popleft()
-    print(node+1, end=' ')
+# BFS 구현 (deque를 활용)
+bfs_result = []
+visited_bfs = [False] * (N + 1)
+queue = deque([V])
+visited_bfs[V] = True
 
-    for next_node in node_list[node]:
-        if visited[next_node]:
-            continue
-        q.append(next_node)
-        visited[next_node] = True
+while queue:
+    current = queue.popleft()
+    bfs_result.append(current)
+    for neighbor in graph[current]:
+        if not visited_bfs[neighbor]:
+            visited_bfs[neighbor] = True
+            queue.append(neighbor)
+
+# 결과 출력
+print(" ".join(map(str, dfs_result)))
+print(" ".join(map(str, bfs_result)))
