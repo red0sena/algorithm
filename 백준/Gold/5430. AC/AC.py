2e7df7d3a8b5b50c814a7bf59761299133829325
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-04-09 02:21:32 +0900

    [Gold V] Title: AC, Time: 1228 ms, Memory: 113516 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.py
+++ 백준/Gold/5430. AC/AC.py
@@ -1,32 +1,33 @@
 from collections import deque
 import sys
 
-input = lambda: sys.stdin.readline().rstrip()
-# sys.stdin = open('input.txt')
+input = lambda:sys.stdin.readline().rstrip()
+
 t = int(input())
 
 for _ in range(t):
     p = input()
     n = int(input())
     input_list = deque(eval(input()))
-    error_flag = False      # 에러 발생 여부를 추적하는 플래그
-    reversed_flag = False   # 리스트의 순서 상태
-
+    point = True
+    error_flag = False
     for command in p:
         if command == "R":
-            reversed_flag = not reversed_flag
+            point = not point
         elif command == "D":
-            if input_list:
-                if not reversed_flag:
+            try:
+                if point:
                     input_list.popleft()
                 else:
                     input_list.pop()
-            else:
+            except:
                 print('error')
                 error_flag = True
-                break  # 에러 발생 시 반복문 종료
+                break
 
     if not error_flag:
-        if reversed_flag:
+        if not point:
             input_list.reverse()
+
         print("[" + ",".join(map(str, input_list)) + "]")
+
