c6563688a7b7339b8b0a28cf9a898fab1c9e9db2
KimKyungHwan <106056971+red0sena@users.noreply.github.com>
2025-04-08 15:37:27 +0900

    [level 3] Title: 거스름돈, Time: 412.42 ms, Memory: 12.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,16 @@
+def solution(n, money):
+    # dp[i]는 i원을 만드는 경우의 수를 저장
+    dp = [0] * (n + 1)
+    dp[0] = 1  # 0원을 만드는 경우는 1가지
+    
+    # 돈의 종류를 하나씩 사용하면서 dp 배열 채우기
+    for coin in money:
+        for amount in range(coin, n + 1):
+            dp[amount] += dp[amount - coin]
+    
+    return dp[n]
+
+# 테스트 예시
+n = 5
+money = [1, 2, 5]
+print(solution(n, money))  # 출력: 4
