9f73f42afc065ec850ea06fbf52019d366ebb055
KimKyunghwan <106056971+red0sena@users.noreply.github.com>
2025-05-22 15:02:49 +0900

    [Gold IV] Title: 이분 그래프, Time: 1332 ms, Memory: 54012 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,38 +1,46 @@
-from collections import deque
 import sys
-input = sys.stdin.readline
+from collections import deque
 
-def is_bipartite(v, graph, color, start):
-    q = deque([start])
-    color[start] = 1  # 시작 정점 색 1
+
+k = int(sys.stdin.readline().rstrip())
+
+
+def bfs(v, graph, color, i):
+    q = deque([(i, 1)])
+    color[i] = 1
     while q:
-        u = q.popleft()
+        u, now_color = q.popleft()
         for v in graph[u]:
-            if color[v] == 0:  # 미방문 정점
-                color[v] = -color[u]  # 반대 색 부여
-                q.append(v)
-            elif color[v] == color[u]:  # 색 충돌
-                return False
+
+            if color[v] == 0:
+                next_color =  now_color * -1
+                color[v] = next_color
+                q.append((v, next_color))
+
+            else:
+                if color[v] == color[u]:
+                    return False
     return True
 
-k = int(input())
+
+
 for _ in range(k):
-    v, e = map(int, input().split())
+    v, e = map(int, sys.stdin.readline().rstrip().split())
     graph = [[] for _ in range(v + 1)]
     color = [0] * (v + 1)
-    
-    # 간선 입력
     for _ in range(e):
-        a, b = map(int, input().split())
+        a, b = map(int, sys.stdin.readline().rstrip().split())
         graph[a].append(b)
         graph[b].append(a)
-    
-    # 모든 정점에 대해 BFS
+
     result = True
     for i in range(1, v + 1):
         if color[i] == 0:  # 미방문 정점에서 BFS 시작
-            if not is_bipartite(v, graph, color, i):
+            if not bfs(v, graph, color, i):
                 result = False
                 break
-    
-    print("YES" if result else "NO")
+
+    if result:
+        print("YES")
+    else:
+        print("NO")
