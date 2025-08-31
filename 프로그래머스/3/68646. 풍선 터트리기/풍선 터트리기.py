b8e032105dd8ff3b29f261176d1046a324ad3453
KimKyunghwan <106056971+red0sena@users.noreply.github.com>
2025-08-31 14:27:48 +0900

    [level 3] Title: 풍선 터트리기, Time: 790.04 ms, Memory: 63 MB -BaekjoonHub

--- 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
+++ 프로그래머스/3/68646. 풍선 터트리기/풍선 터트리기.py
@@ -1,16 +1,21 @@
 def solution(a):
     n = len(a)
-    if n == 0:
-        return 0
-    inf = 10**10
+    
+    inf = float('inf')
+    # 현재 index 기준 왼쪽에 풍선 중에서 최소값 구하기
     left_min = [inf] * n
     for i in range(1, n):
         left_min[i] = min(left_min[i-1], a[i-1])
+		
+		# 현재 index 기준 오른쪽의 풍선 중에서 최소값 구하기
     right_min = [inf] * n
     for i in range(n-2, -1, -1):
         right_min[i] = min(right_min[i+1], a[i+1])
+    
+    # 마지막까지 남을 수 있는 풍선 수 세기
     count = 0
     for i in range(n):
-        if not (left_min[i] < a[i] and right_min[i] < a[i]):
+		    # 왼쪽 최소값보다 작거나 오른쪽 최소값보다 작으면 살 수 있다.
+        if a[i] <= left_min[i] or a[i] <= right_min[i]:
             count += 1
     return count
