f72c2267f683c2dbde932af4d19ff4215e9c7ac7
KimKyunghwan <106056971+red0sena@users.noreply.github.com>
2025-10-04 17:07:31 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 4332 ms, Memory: 37428 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,21 +1,31 @@
 import sys
-input = sys.stdin.readline
 
-N = int(input())
-matrix = [list(map(int, input().split())) for _ in range(N)]
-DP = [[0]*N for _ in range(N)]
+def solve():
+    N = int(sys.stdin.readline())
+    matrices = []
+    for _ in range(N):
+        matrices.append(list(map(int, sys.stdin.readline().split())))
 
-# 분할된 그룹의 크기를 1부터 N-1까지 돎
-for size in range(1, N):
-	# 크기 size인 그룹의 모든 경우의 수 돎
-    for start in range(N - size):
-        end = start + size
-        
-        # 어떤 그룹의 최소 곱셈 횟수는 분할한 두 그룹의 최소 곱셈 횟수 + 각 그룹의 곱셈 다 끝나고 남은 행렬끼리의 곱셈 횟수
-        result = float("inf")
-        for cut in range(start, end):
-            result = min(result, DP[start][cut] + DP[cut+1][end] +
-                        matrix[start][0]*matrix[cut][1]*matrix[end][1])
-        DP[start][end] = result
+    # DP 테이블 초기화
+    # dp[i][j]: i번째 행렬부터 j번째 행렬까지 곱하는 데 필요한 최소 연산 횟수
+    dp = [[0] * N for _ in range(N)]
 
-print(DP[0][-1])
+    # len: 곱하는 행렬의 개수 (체인의 길이)
+    for length in range(1, N):
+        # i: 시작 행렬의 인덱스
+        for i in range(N - length):
+            # j: 끝 행렬의 인덱스
+            j = i + length
+            
+            # dp[i][j]를 큰 값으로 초기화
+            dp[i][j] = float('inf')
+            
+            # k: 분할 지점
+            for k in range(i, j):
+                cost = dp[i][k] + dp[k+1][j] + matrices[i][0] * matrices[k][1] * matrices[j][1]
+                if dp[i][j] > cost:
+                    dp[i][j] = cost
+
+    print(dp[0][N-1])
+
+solve()
