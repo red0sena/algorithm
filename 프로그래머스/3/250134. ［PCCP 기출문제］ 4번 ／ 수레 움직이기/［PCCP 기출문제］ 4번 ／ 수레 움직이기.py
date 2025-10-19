c9064c1e5b4f504d33559d0e866f62ba56fcc213
KimKyunghwan <106056971+red0sena@users.noreply.github.com>
2025-10-19 18:08:42 +0900

    [level 3] Title: [PCCP 기출문제] 4번 / 수레 움직이기, Time: 33.74 ms, Memory: 13.9 MB -BaekjoonHub

--- 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
+++ 프로그래머스/3/250134. ［PCCP 기출문제］ 4번 ／ 수레 움직이기/［PCCP 기출문제］ 4번 ／ 수레 움직이기.py
@@ -1,84 +1,80 @@
-import collections
+from collections import deque
+
+WALL = 5
+RED_START, BLUE_START, RED_END, BLUE_END = 1, 2, 3, 4
 
 def solution(maze):
-    n = len(maze)
-    m = len(maze[0])
-    
-    # 각 수레의 시작점과 도착점 좌표 찾기
-    red_start, blue_start = [], []
-    red_end, blue_end = [], []
-    
+    n, m = len(maze), len(maze[0])
+
+    # 시작 / 도착 위치 찾기
+    red_s = blue_s = red_e = blue_e = None
     for r in range(n):
         for c in range(m):
-            if maze[r][c] == 1: red_start = [r, c]
-            elif maze[r][c] == 2: blue_start = [r, c]
-            elif maze[r][c] == 3: red_end = [r, c]
-            elif maze[r][c] == 4: blue_end = [r, c]
+            v = maze[r][c]
+            if v == RED_START: red_s = (r, c)
+            elif v == BLUE_START: blue_s = (r, c)
+            elif v == RED_END: red_e = (r, c)
+            elif v == BLUE_END: blue_e = (r, c)
 
-    # (r, c) 좌표를 비트로 변환하는 함수
-    def pos_to_bit(r, c):
-        return 1 << (r * m + c)
+    dirs = [(-1,0), (1,0), (0,-1), (0,1)]
 
-    # BFS를 위한 큐 초기화
-    # 상태: (빨간_r, 빨간_c, 파란_r, 파란_c, 빨간_마스크, 파란_마스크, 턴_수)
-    q = collections.deque([(
-        red_start[0], red_start[1], 
-        blue_start[0], blue_start[1], 
-        pos_to_bit(red_start[0], red_start[1]), 
-        pos_to_bit(blue_start[0], blue_start[1]), 
-        0
-    )])
-    
-    # 방문 기록을 위한 set: (빨간_r, 빨간_c, 파란_r, 파란_c, 빨간_마스크, 파란_마스크)
-    visited = set()
-    visited.add((
-        red_start[0], red_start[1], 
-        blue_start[0], blue_start[1], 
-        pos_to_bit(red_start[0], red_start[1]), 
-        pos_to_bit(blue_start[0], blue_start[1])
-    ))
-    
-    dr = [-1, 1, 0, 0]
-    dc = [0, 0, -1, 1]
+    # 각 말의 방문 경로를 2D 리스트로 관리하기 위해, 상태를 visited set으로 묶음
+    # visited = {(red_pos, blue_pos, tuple of red visited map, tuple of blue visited map)}
+    def copy_board(board):
+        # 방문 배열을 불변형(tuple of tuple)으로 만들어 visited에 넣기 위해
+        return tuple(tuple(row) for row in board)
+
+    # 초기 방문 상태
+    red_visited = [[False]*m for _ in range(n)]
+    blue_visited = [[False]*m for _ in range(n)]
+    rr, rc = red_s
+    br, bc = blue_s
+    red_visited[rr][rc] = True
+    blue_visited[br][bc] = True
+
+    q = deque([(red_s, blue_s, red_visited, blue_visited, 0)])
+    visited = {(red_s, blue_s, copy_board(red_visited), copy_board(blue_visited))}
 
     while q:
-        rr, rc, br, bc, r_mask, b_mask, count = q.popleft()
+        (rr, rc), (br, bc), rmap, bmap, turns = q.popleft()
 
-        if rr == red_end[0] and rc == red_end[1] and br == blue_end[0] and bc == blue_end[1]:
-            return count
+        if (rr, rc) == red_e and (br, bc) == blue_e:
+            return turns
 
-        # 모든 이동 조합 탐색
-        for i in range(4):
-            is_red_at_end = (rr == red_end[0] and rc == red_end[1])
-            nrr, nrc = (rr, rc) if is_red_at_end else (rr + dr[i], rc + dc[i])
+        for dr_r, dc_r in dirs:
+            # 빨강 이동
+            if (rr, rc) == red_e:
+                nR = (rr, rc)
+            else:
+                nR = (rr + dr_r, rc + dc_r)
+            if not (0 <= nR[0] < n and 0 <= nR[1] < m): continue
+            if maze[nR[0]][nR[1]] == WALL: continue
+            if (rr, rc) != red_e and rmap[nR[0]][nR[1]]: continue
 
-            for j in range(4):
-                is_blue_at_end = (br == blue_end[0] and bc == blue_end[1])
-                nbr, nbc = (br, bc) if is_blue_at_end else (br + dr[j], bc + dc[j])
+            for dr_b, dc_b in dirs:
+                # 파랑 이동
+                if (br, bc) == blue_e:
+                    nB = (br, bc)
+                else:
+                    nB = (br + dr_b, bc + dc_b)
+                if not (0 <= nB[0] < n and 0 <= nB[1] < m): continue
+                if maze[nB[0]][nB[1]] == WALL: continue
+                if (br, bc) != blue_e and bmap[nB[0]][nB[1]]: continue
 
-                # 1. 격자 밖 또는 벽 체크
-                if not (0 <= nrr < n and 0 <= nrc < m and maze[nrr][nrc] != 5): continue
-                if not (0 <= nbr < n and 0 <= nbc < m and maze[nbr][nbc] != 5): continue
+                # 충돌 / 교차 금지
+                if nR == nB: continue
+                if nR == (br, bc) and nB == (rr, rc): continue
 
-                # 2. ❗❗ (핵심 수정) 각 수레가 자신의 방문했던 칸으로 이동하는지 체크
-                # 단, 도착해서 멈춰있는 경우는 예외
-                if not is_red_at_end and (r_mask & pos_to_bit(nrr, nrc)): continue
-                if not is_blue_at_end and (b_mask & pos_to_bit(nbr, nbc)): continue
+                # 새 방문 배열 복사
+                new_rmap = [row[:] for row in rmap]
+                new_bmap = [row[:] for row in bmap]
+                new_rmap[nR[0]][nR[1]] = True
+                new_bmap[nB[0]][nB[1]] = True
 
-                # 3. 두 수레가 같은 칸으로 이동 (충돌)
-                if nrr == nbr and nrc == nbc: continue
-                
-                # 4. 두 수레가 자리를 교체
-                if nrr == br and nrc == bc and nbr == rr and nbc == rc: continue
+                state = (nR, nB, copy_board(new_rmap), copy_board(new_bmap))
+                if state in visited:
+                    continue
+                visited.add(state)
+                q.append((nR, nB, new_rmap, new_bmap, turns + 1))
 
-                # 새로운 방문 마스크 계산
-                nr_mask = r_mask | pos_to_bit(nrr, nrc)
-                nb_mask = b_mask | pos_to_bit(nbr, nbc)
-
-                # 5. 현재 위치와 방문 기록(마스크)이 모두 동일한 상태를 방문했는지 체크
-                if (nrr, nrc, nbr, nbc, nr_mask, nb_mask) in visited: continue
-                
-                visited.add((nrr, nrc, nbr, nbc, nr_mask, nb_mask))
-                q.append((nrr, nrc, nbr, nbc, nr_mask, nb_mask, count + 1))
-                
     return 0
